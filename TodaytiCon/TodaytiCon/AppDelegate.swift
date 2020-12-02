f4abc385d57f1d2534a72454e2d1ce94bbcd8b1b
JeongChangYong <ckddyd1202@daum.net>
2020-12-02 20:21:07 +0900

    Create: First-Setting
    초기 프로젝트 생성 및 Alamofire 설치

---
+++ TodaytiCon/TodaytiCon/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TodaytiCon
+//
+//  Created by 정창용 on 2020/12/02.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ TodaytiCon/TodaytiCon/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodaytiCon
+//
+//  Created by 정창용 on 2020/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
